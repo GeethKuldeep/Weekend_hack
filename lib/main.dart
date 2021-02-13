4aee0b5bfe2800e48b89bffe524aa0ab68597699
GeethKuldeep <geethkuldeep@gmail.com>
2021-02-13 18:26:42 +0530

    Push notifications added!!!

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,35 @@
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:hack/screens/home_screen.dart';
 import 'package:hack/screens/landing_screen.dart';
 import 'package:hack/screens/welcome_screen.dart';
+import 'package:timezone/data/latest.dart' as tz;
 
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
 
 void main() async{
+  tz.initializeTimeZones();
   WidgetsFlutterBinding.ensureInitialized();
+
+  const AndroidInitializationSettings initializationSettingsAndroid = AndroidInitializationSettings('codex_logo');
+
+  final InitializationSettings initializationSettings = InitializationSettings(
+      android: initializationSettingsAndroid);
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+      onSelectNotification:(String payload )async{
+        if(payload != null){
+          debugPrint('notification payload' +payload);
+        }
+      } );
+
   await Firebase.initializeApp();
-  runApp(Crypto());
+  runApp(hack());
 }
 
-class Crypto extends StatelessWidget {
+class hack extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/screens/notifications_screen.dart
+++ lib/screens/notifications_screen.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/data/latest_all.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
+
+
+import '../Timezone.dart';
+import '../main.dart';
+
+
+
 class Notify extends StatefulWidget {
 
   @override
@@ -22,6 +32,7 @@ class _NotifyState extends State<Notify> {
         });
         keys.addAll(result.data().keys);
         values.addAll(result.data().values);
+        print(values);
 
 
       });
@@ -40,27 +51,58 @@ class _NotifyState extends State<Notify> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: ListView.builder(
-            itemCount: length,
-            itemBuilder: (context,index){
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Card(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Column(
+        body:ListView.builder(
+                itemCount: length,
+                itemBuilder: (context,index){
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Text(keys[index]),
-                          Text('${values[index].toDate()}'),
+                          Column(
+                            children: [
+                              Text(keys[index]),
+                              Text('${values[index].toDate()}'),
+                              RaisedButton(
+                                  child:Text('notify'),onPressed: (){
+                                scheduleAlarm();
+                              })
+                            ],
+                          )
                         ],
-                      )
-                    ],
-                  ),
-                ),
-              );
-            }),
+                      ),
+                    ),
+                  );
+                }),
+
       ),
     );
+  }
+
+  void scheduleAlarm() async {
+    final timeZone = TimeZone();
+    String timeZoneName = await timeZone.getTimeZoneName();
+    print(timeZoneName);
+    print('0');
+     tz.initializeTimeZones();
+    print('0.1');
+     await tz.setLocalLocation(await tz.getLocation(timeZoneName));
+     print(tz.local);
+    print(tz.local);
+     var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+
+      'alarm_notif',
+      'alarm_notif',
+      'Channel for Alarm notification',
+      icon: 'codex_logo',
+      sound: RawResourceAndroidNotificationSound('a_long_cold_sting'),
+      largeIcon: DrawableResourceAndroidBitmap('codex_logo'),
+    );
+     print('2');
+    var platformChannelSpecifics =NotificationDetails( android: androidPlatformChannelSpecifics);
+     print('3');
+    await flutterLocalNotificationsPlugin.zonedSchedule(0, keys[0], 'going start',tz.TZDateTime.from(values[0].toDate(), tz.local), platformChannelSpecifics, androidAllowWhileIdle: true,);
+     print('4');
   }
 }
